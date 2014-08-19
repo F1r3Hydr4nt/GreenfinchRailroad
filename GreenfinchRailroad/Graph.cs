cc959dacc1ddfe345cf78bff42e4696f3af95d27
freddiehonohan <honohanf@gmail.com>
2014-08-19 12:30:14 +0100

    First Commit
    Basic setup initialising graph object with input, storing cities in list
    and routes in adjacency matrix

---
+++ GreenfinchRailroad/Graph.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenfinchRailroad
+{
+    class Graph
+    {
+        public List<City> cities;
+        public int[,] routes;//stores null if no route, or distance if a route exists
+        public Graph(int n)
+        {
+            cities = new List<City>();
+            routes = new int[n, n];
+        }
+        public void PopulateGraph(string[] connectingCities)
+        {
+            //for every city pair route string extract cities and distance ensuring no duplicate cities are added to the list
+            foreach (string s in connectingCities)
+            {
+                char firstCity = s[0];
+                char secondCity = s[1];
+                int distance = int.Parse(s[2].ToString());
+                int firstIndex = FindCity(firstCity);
+                int secondIndex = FindCity(secondCity);
+                if (firstIndex == -1)
+                {
+                    cities.Add(new City(firstCity));
+                    firstIndex = FindCity(firstCity);
+                }
+                if (secondIndex == -1)
+                {
+                    cities.Add(new City(secondCity));
+                    secondIndex = FindCity(secondCity);
+                }
+                AddRoute(firstIndex, secondIndex, distance);
+            }
+            foreach (City c in cities)
+            {
+                Program.print(c.name.ToString());
+            }
+        }
+        public void PrintAdjacencyMatrix()
+        {
+            Console.WriteLine("Adjacency Matrix");
+            int cityCount = cities.Count;
+            for (int i = 0; i < cityCount; i++)
+            {
+                string line = "";
+                for (int j = 0; j < cityCount; j++) line += routes[i, j].ToString() + " ";
+                Console.WriteLine(line);
+            }
+        }
+        //returns index of city or -1 if not found
+        public int FindCity(char c)
+        {
+            int index = -1;
+            for (int i = 0; i < cities.Count; i++)
+                if (cities[i].name == c) index = i;
+            return index;
+        }
+        //Routes are one way only
+        public void AddRoute(int city1, int city2, int distance)
+        {
+            routes[city1, city2] = distance;
+        }
+    }
+}
---
+++ GreenfinchRailroad/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenfinchRailroad
+{
+    class Program
+    {
+        static bool isDebug = true;
+        public static void print(string s)
+        {
+            if (isDebug) Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            //Graph object instantiated with total amount of cities (assumption: n is known)
+            Graph graph = new Graph(5);
+            string inputData = "AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7";
+            string[] connectingCities = inputData.Split(',');
+            graph.PopulateGraph(connectingCities);
+            graph.PrintAdjacencyMatrix();
+            Console.Read();
+        }
+    }
+
+    struct City
+    {
+        public char name;
+        public City(char c)
+        {
+            name = c;
+        }
+    }
+
+  
+}
